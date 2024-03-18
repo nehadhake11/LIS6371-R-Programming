7a72b0cd680879a55763865658cf081f5215e6d4
nehadhake11 <nehadhake@usf.edu>
2024-03-17 23:48:59 -0400

    Project init

---
+++ StructBioinfo/R/hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+ # Install the roxygen2 package
+ install.packages("roxygen2")
+
+ # Load the roxygen2 package
+ library(roxygen2)
